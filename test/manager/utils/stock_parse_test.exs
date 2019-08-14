a8bb5fd4544939696d6fc69ae346cb1f1ad4c2a9
Guilherme Dutra <guipereira07@gmail.com>
2019-08-13 21:35:06 -0300

    change name from price_parse to stock_parse

---
+++ test/manager/utils/stock_parse_test.exs
@@ -0,0 +1,15 @@
+defmodule Cleverbot.Manager.Utils.StockParseTest do
+  use ExUnit.Case
+  alias Cleverbot.Manager.Utils.StockParse
+
+  test "convert streaming data from string to Stock" do
+    stock_data = [
+      1002,
+      nil,
+      [218, "1.34543425", "1.34421299", "1.23475671", "0.06616981", "5744.01346989",
+        "4629.36825592", 0, "1.34543425", "1.22000000"]
+    ]
+
+    Cleverbot.Manager.Utils.StockParse.parse(stock_data) == %{currency_code: 218, price: 1.34543425}
+  end
+end
